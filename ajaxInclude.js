230f835e546987850b3eb503a6d0f94dda6eebce
幻想家 <ferron@163.com>
2015-10-10 15:54:41 +0800

    Update ajaxInclude.js
    修改为 jquery 插件 方式，可以向常见的jquery插件那样调用。

--- ajaxInclude.js
+++ ajaxInclude.js
@@ -1,23 +1,33 @@
 /**
  * Created by 幻想家 on 2015/7/2.
  * ajaxInclude方法。
+ * 版本更新：201509230909（采用jquery插件的方式写）
+ * 使用方法：
+ <script id="ieAlert" type="text/javascript">
+    $('#ieAlert').ajaxInclude('inc_ieAlert.html');
+ </script>
+ * 更新须知：之前的版本会导致页面因为load方法，多次触发页面中$(document).ready();导致其内部的方法被多次触发，那个带按钮的计算价格的
+ * 输入框组件就是例证，这次的更新不知道能不能解决该问题，有待验证！（猜测还是解决不了）2015.9.23.9:23
+ * add：问题已解决，不知道是这个方法插件化，还是那个计算价格组件的方法插件化搞好的。
+ *
+ * 版本：v201509111553
  * 模拟php的include功能，通过ajax load()实现。
  * 注意：只能在网站环境下运行，在文件夹中直接双击运行是会报错的。
- * a:代表当前script节点的id
- * b:代表引入的文件，可以包括路径
+ * scriptID:代表当前script节点的id
+ * includeFile:代表引入的文件，可以包括路径
  * 使用方法：
  * <head>中引入：
-   <script src="js/jquery.ajaxInlcude.js"></script>
+ <script type="text/javascript" src="js/jquery.ajaxInlcude.js"></script>
  * 需要的位置写它：(id可以自定义，但不能重复哦)
-   <script id="CustomIDNotSame" type="text/javascript">
-     $(document).ready(
-       ajaxInclude('CustomIDNotSame','head.html')
-     );
-   </script>
+ <script id="CustomIDNotSame" type="text/javascript">
+ $(document).ready(
+ ajaxInclude('CustomIDNotSame','head.html')
+ );
+ </script>
  * 代码可以更简单一点：
-    <script id="CustomIDNotSame" type="text/javascript">
-      $(ajaxInclude('CustomIDNotSame','head.html'));
-    </script>
+ <script id="CustomIDNotSame" type="text/javascript">
+ $(ajaxInclude('CustomIDNotSame','head.html'));
+ </script>
  * 在需要引入的地方，书写上面的代码。
  * 1、需要给script节点添加id，这方法时通过id来判断include到的页面位置的。
  * 2、一个页面多次应用，id一定不要重复。
@@ -29,20 +39,43 @@
 
 ;
 
-function ajaxInclude(a,b){
-    //通过js创建外层div，位置是当前script节点前方，所以一个页面中多次引用时需要使用不同的id
-    $("#"+a).before('<div id='+a+'-wrap'+'></div>');
-    //在创建的div中，通过ajax load()引入指定html文件
-    //添加回调函数，处理一些问题并捕捉异常
-    $("#"+a+'-wrap').load(b,function(responseTxt,statusTxt,xhr){
-        if(statusTxt=="success") {
-            //可以在这里删除有碍观瞻的自身script节点，表担心，不影响函数执行
-            $(this).next().remove();
-            //没错，这就是传说中的去壳大法！
-            $(this).children().unwrap();
-        }else if(statusTxt=="error") {
-            $(this).html("<h1 style='font-size: 50px'>此处组件加载失败！</h1>");
-            alert("Error: " + xhr.status + ": " + xhr.statusText);
-        }
-    });
-}
+(function($) {
+    $.fn.ajaxInclude = function(includeFile) {
+        var scriptID = $(this).attr('id');
+        //通过js创建外层div，位置是当前script节点前方，所以一个页面中多次引用时需要使用不同的id
+        $(this).before('<div id='+scriptID+'-tempWrap'+'></div>');
+        //在创建的div中，通过ajax load()引入指定html文件
+        //添加回调函数，处理一些问题并捕捉异常
+        $("#"+scriptID+'-tempWrap').load(includeFile,function(responseTxt,statusTxt,xhr){
+            if(statusTxt=="success") {
+                //可以在这里删除有碍观瞻的自身script节点，表担心，不影响函数执行
+                $(this).next().remove();
+                //没错，这就是传说中的去壳大法！
+                $(this).children().unwrap();
+            }else if(statusTxt=="error") {
+                $(this).html("<h1 style='font-size: 25px'>此处组件加载失败！</h1>");
+                console.log("ajaxInclude.js执行错误————"+xhr.status + "：" + xhr.statusText);
+            }
+        });
+    };
+})(jQuery);
+
+//;
+
+//function ajaxInclude(scriptID,includeFile){
+//    //通过js创建外层div，位置是当前script节点前方，所以一个页面中多次引用时需要使用不同的id
+//    $("#"+scriptID).before('<div id='+scriptID+'-tempWrap'+'></div>');
+//    //在创建的div中，通过ajax load()引入指定html文件
+//    //添加回调函数，处理一些问题并捕捉异常
+//    $("#"+scriptID+'-tempWrap').load(includeFile,function(responseTxt,statusTxt,xhr){
+//        if(statusTxt=="success") {
+//            //可以在这里删除有碍观瞻的自身script节点，表担心，不影响函数执行
+//            $(this).next().remove();
+//            //没错，这就是传说中的去壳大法！
+//            $(this).children().unwrap();
+//        }else if(statusTxt=="error") {
+//            $(this).html("<h1 style='font-size: 25px'>此处组件加载失败！</h1>");
+//            alert("Error: " + xhr.status + ": " + xhr.statusText);
+//        }
+//    });
+//}
